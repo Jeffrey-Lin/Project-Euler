e7fa597df3cbd2e9c39ad63c4337a44443f8d874
Jeffrey Lin <lin.jeffrey@hotmail.com>
2016-07-31 17:39:19 -0500

    Implement problems 1, 2, & 4

---
+++ Problem4.swift
@@ -0,0 +1,30 @@
+// Project Euler: Problem 4
+//
+// A palindromic number reads the same both ways.
+// The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+// Find the largest palindrome made from the product of two 3-digit numbers.
+//
+// Author: Jeffrey Lin
+
+private func isPalindrome(num: String) -> Bool {
+    return num == String(num.characters.reverse())
+}
+
+var largestPalindrome = 0
+var max = 999
+
+firstLoop: while max > 500 {
+    var second_max = 999
+    secondLoop: while second_max > 0 {
+        if isPalindrome(String(second_max * max)) {
+            if second_max * max > largestPalindrome {
+                largestPalindrome = second_max * max
+            }
+        }
+        second_max -= 1
+    }
+
+    max -= 1
+}
+
+print(largestPalindrome)
